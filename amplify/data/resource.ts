70e8dd8e663b524d28509ae39d31b840212cb66c
icihr0roie <ichir0roie@gmail.com>
2025-06-21 22:02:52 +0900

    add amplify

---
+++ amplify/data/resource.ts
@@ -0,0 +1,24 @@
+import { type ClientSchema, a, defineData } from "@aws-amplify/backend";
+
+const schema = a.schema({
+  Todo: a
+    .model({
+      content: a.string(),
+    })
+    .authorization((allow) => [allow.guest()]),
+  Settings: a
+    .model({
+      key: a.string(),
+      value: a.string(),
+    })
+    .authorization((allow) => [allow.authenticated()]),
+});
+
+export type Schema = ClientSchema<typeof schema>;
+
+export const data = defineData({
+  schema,
+  authorizationModes: {
+    defaultAuthorizationMode: "identityPool",
+  },
+});
